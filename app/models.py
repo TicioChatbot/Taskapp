08df76d5e412601f0631b9a57537b17b9c6d1cad
Jacobo <ticiochatbo@gmail.com>
2024-06-17 20:53:19 -0500

    bugfix for ptcg on time, the parentisis location is important

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class User(db.Model):
         terminadas = [task for task in self.backref if task.complete == True]
         on_time = [task for task in terminadas if task.overdue == False]
         if len(terminadas) > 0: 
-            return f'{floor(len(on_time)/len(terminadas)) * 100} %'
+            return f'{floor(len(on_time)/len(terminadas)* 100)} %'
         else: 
             return '0%'
     
@@ -57,6 +57,9 @@ class Task(db.Model):
     complete = db.Column(db.Boolean, default=False)
     overdue = db.Column(db.Boolean, default=False)
 
+    def __repr__(self):
+        return self.content
+
     def __lt__(self, other): 
         return self.duedate < other.duedate
 
@@ -77,5 +80,8 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(500))
     tasks = db.relationship('Task', backref='project')
+
+    def __repr__(self):
+        return self.name
 
 
